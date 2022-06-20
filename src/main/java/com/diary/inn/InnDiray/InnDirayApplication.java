3f7cc32a722e6480e0b0c22d0822b50c496a9844
Mlink <lmo9903@gmail.com>
2022-06-21 00:52:51 +0900

    Inn init setting of project

---
+++ src/main/java/com/diary/inn/InnDiray/InnDirayApplication.java
@@ -0,0 +1,13 @@
+package com.diary.inn.InnDiray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnDirayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnDirayApplication.class, args);
+	}
+
+}
