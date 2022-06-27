6c113c9af30130195b65e45c84787d60814eb8bf
Mlink <lmo9903@gmail.com>
2022-06-28 02:30:19 +0900

    delete of old oauth2 service, need to start again

--- src/main/java/com/diary/inn/InnDiray/oauth/exception/OAuthProviderMissMatchException.java
+++
@@ -1,8 +0,0 @@
-package com.diary.inn.InnDiray.oauth.exception;
-
-public class OAuthProviderMissMatchException extends RuntimeException{
-
-    public OAuthProviderMissMatchException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/diary/inn/InnDiray/user/domain/ProviderType.java
+++
@@ -1,9 +0,0 @@
-package com.diary.inn.InnDiray.user.domain;
-
-import lombok.Getter;
-
-@Getter
-public enum ProviderType {
-    GOOGLE,
-    KAKAO
-}
