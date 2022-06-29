2c80ccabc2549e17f4d70375af94b00b991d15f9
Mlink <lmo9903@gmail.com>
2022-06-29 16:29:51 +0900

    old file restore for use

--- src/main/java/com/diary/inn/InnDiary/config/auth/SecurityConfig.java
+++ src/main/java/com/diary/inn/InnDiary/config/auth/SecurityConfig.java
@@ -1,4 +1,15 @@
 package com.diary.inn.InnDiary.config.auth;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http;
+
+        return http.build();
+    }
 }
---
+++ src/test/java/com/diary/inn/InnDiray/InnDirayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diary.inn.InnDiray;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnDirayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
