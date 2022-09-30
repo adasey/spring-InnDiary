f2decdee394abb05ef0077708669ab4d1d0b054c
Mlink <lmo9903@gmail.com>
2022-09-30 23:05:22 +0900

    [feat] deploy 전용 branch

--- src/main/java/com/diary/inn/InnDiary/InnDiaryApplication.java
+++ src/main/java/com/diary/inn/InnDiary/InnDiaryApplication.java
@@ -2,13 +2,20 @@ package com.diary.inn.InnDiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
 //@EnableConfigurationProperties(AppProperties.class)
-public class InnDiaryApplication {
+public class InnDiaryApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(InnDiaryApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnDiaryApplication.class, args);
