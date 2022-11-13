db3a36beb8383e787f54b52a34a545da022c2e2c
이민욱 <lmo9903@gmail.com>
2022-11-14 03:22:51 +0900

    [fix] main에서 refactor 브런치로 변경 하면서 생긴 문제

--- src/main/java/com/diary/inn/InnDiary/InnDiaryApplication.java
+++ src/main/java/com/diary/inn/InnDiary/InnDiaryApplication.java
@@ -1,24 +1,16 @@
 package com.diary.inn.InnDiary;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-//@EnableConfigurationProperties(AppProperties.class)
 public class InnDiaryApplication extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
 		return applicationBuilder.sources(InnDiaryApplication.class);
 	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(InnDiaryApplication.class, args);
-	}
-
 }
